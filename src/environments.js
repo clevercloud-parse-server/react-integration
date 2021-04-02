155e313c59d24628e594467de3af69174a29f049
Laurent Doguin <laurent.doguin@gmail.com>
2021-04-02 10:42:17 +0200

    add env var

--- src/environments.js
+++ src/environments.js
@@ -1,5 +1,5 @@
 module.exports = {
-    APPLICATION_ID: "MyTest",
-    JAVASCRIPT_KEY: "JAVASCRIPT_KEY",
-    SERVER_URL: "https://ldoguin-parse.cleverapps.io/parse"
+    APPLICATION_ID: process.env.REACT_APP_PARSE_APP_ID,
+    JAVASCRIPT_KEY: process.env.REACT_APP_APP_JAVASCRIPT_KEY,
+    SERVER_URL: process.env.REACT_APP_SERVER_URL
 }
